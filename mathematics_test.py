bcf1263207f49b58ba6bf47a0064854c0a9b11c6
Teemu Hujanen <teemu.hujanen@protonmail.com>
2023-11-29 11:58:52 +0200

    Fixed test files

--- mathematics_test.py
+++ mathematics_test.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 def add(x, y):
     return x + y
 
@@ -11,17 +10,3 @@ def power(x, y):
     for _ in range(int(y)):
         result *= x
     return result
-=======
-def add(x, y):
-    return x + y
-
-def multiply(x, y):
-    return x * y
-
-def power(x, y):
-    """Calculate x raised to the power y without using math.pow."""
-    result = 1
-    for _ in range(int(y)):
-        result *= x
-    return result
->>>>>>> 0e137f2d041d6b1d670bd01802e5f5c2b9dec48a
--- mathematicstests.py
+++ mathematicstests.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import unittest
 import mathematics
 
@@ -15,22 +14,4 @@ class TestMathMethods(unittest.TestCase):
         self.assertEqual(256, mathematics.power(2, 8))
 
 if __name__ == '__main__':
-=======
-import unittest
-import mathematics
-
-class TestMathMethods(unittest.TestCase):
-
-    def test_add(self):
-        self.assertEqual(3, mathematics.add(1, 2))
-        self.assertEqual(10, mathematics.add(3, 7))
-
-    def test_multiply(self):
-        self.assertEqual(50, mathematics.multiply(5, 10))
-
-    def test_power(self):
-        self.assertEqual(256, mathematics.power(2, 8))
-
-if __name__ == '__main__':
->>>>>>> 0e137f2d041d6b1d670bd01802e5f5c2b9dec48a
     unittest.main()
