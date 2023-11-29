45aa20e303fa2a28f03a90308858cc2acb262951
Teemu Hujanen <teemu.hujanen@protonmail.com>
2023-11-29 11:31:17 +0200

    PyTest tiedostoja

---
+++ mathematics_test.py
@@ -0,0 +1,27 @@
+<<<<<<< HEAD
+def add(x, y):
+    return x + y
+
+def multiply(x, y):
+    return x * y
+
+def power(x, y):
+    """Calculate x raised to the power y without using math.pow."""
+    result = 1
+    for _ in range(int(y)):
+        result *= x
+    return result
+=======
+def add(x, y):
+    return x + y
+
+def multiply(x, y):
+    return x * y
+
+def power(x, y):
+    """Calculate x raised to the power y without using math.pow."""
+    result = 1
+    for _ in range(int(y)):
+        result *= x
+    return result
+>>>>>>> 0e137f2d041d6b1d670bd01802e5f5c2b9dec48a
